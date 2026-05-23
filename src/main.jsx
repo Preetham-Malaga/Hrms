8e5105188875bcfd799514e3fafdc56155375895
Preetham <preetham.malaga@kodefast.net>
2026-05-24 01:15:29 +0530

    Final auth fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { AuthProvider } from "./context/authcontext";
+import { AuthProvider } from "./context/auth";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
