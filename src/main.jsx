4e8bd0e3401dcb83122d351aff15c5707babe4f2
Preetham <preetham.malaga@kodefast.net>
2026-05-24 01:10:03 +0530

    Matched authcontext import

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./context/authcontext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
